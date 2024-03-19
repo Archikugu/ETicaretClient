ac59e07c421a3845215a5a44989304a1703c82f0
Gokhan <engokhangok@gmail.com>
2024-03-19 18:46:18 +0300

    Angular Material integration added.

--- src/app/admin/layout/layout.module.ts
+++ src/app/admin/layout/layout.module.ts
@@ -2,8 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LayoutComponent } from './layout.component';
 import { ComponentsModule } from './components/components.module';
-import { HeaderComponent } from './components/header/header.component';
 import { RouterModule } from '@angular/router';
+import {MatSidenavModule} from '@angular/material/sidenav';
 
 
 
@@ -14,7 +14,8 @@ import { RouterModule } from '@angular/router';
   imports: [
     CommonModule,
     ComponentsModule,
-    RouterModule
+    RouterModule,
+    MatSidenavModule
   ],
   exports:[
     LayoutComponent
