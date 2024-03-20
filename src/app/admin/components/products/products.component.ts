7e102071b3641f98b1bb60ab45cf0810f0b327d7
Gokhan <engokhangok@gmail.com>
2024-03-20 19:03:43 +0300

    added angular bootstrap jquery integration

--- src/app/admin/components/products/products.component.ts
+++ src/app/admin/components/products/products.component.ts
@@ -3,7 +3,8 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrl: './products.component.scss'
+  styleUrl: './products.component.scss',
+  host: {'some-binding': 'some-value'},
 })
 export class ProductsComponent {
 
