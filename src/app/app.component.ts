1edc3d594be44ec70fdbf248711f5e22a6581d3d
Gokhan <engokhangok@gmail.com>
2024-03-21 21:23:05 +0300

    Toastr.js library was added and customized notification tests were conducted.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ToastrService } from 'ngx-toastr';
+import { CustomToastrService, ToastrMessageType, ToastrPosition } from './services/ui/custom-toastr.service';
 // declare var $: any
 
 @Component({
@@ -8,6 +10,17 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'ETicaretClient';
+
+  constructor(private toastrService: CustomToastrService) {
+    toastrService.message("Merhaba", "Gokhan", {
+      messageType: ToastrMessageType.Info,
+      position: ToastrPosition.TopRight,
+    });
+
+    // toastrService.message("Merhaba", "Gokhan",ToastrMessageType.Warning,ToastrPosition.TopLeft);
+
+  }
+
 }
 // $(document).ready(() => {
 //   alert("Test")
